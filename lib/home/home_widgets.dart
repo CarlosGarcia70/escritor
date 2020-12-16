b0b092c87ea8d482bd367f65f481314c9927c322
Carlos Adriano Garcia <carlosadrianogarci@gmail.com>
2020-12-16 16:37:13 -0300

    Aperfeiçoamento do primeiro módulo. Criação do modelo de ideia.

--- lib/home/home_widgets.dart
+++ lib/home/home_widgets.dart
@@ -9,20 +9,33 @@ Widget ListaIdeias(List<ModelIdeia> lista) {
   return StaggeredGridView.countBuilder(
       crossAxisCount: 4,
       itemCount: lista.length,
-      itemBuilder: (BuildContext context, int index) =>
-      new Card(
-        color: Colors.lime,
-        child: InkWell(
-          splashColor: Colors.lime[900],
-          onTap: () {
-            Get.to(EditIdeia(lista[index]));
-          },
-          child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text('${lista[index].texto}')
-          ),
-        )
-      ),
+      itemBuilder: (BuildContext context, int index) {
+        Color cor1 = Colors.lime;
+        Color cor2 = Colors.lime[900];
+        if(lista[index].fase == 1) {
+          cor1 = Colors.lime;
+          cor2 = Colors.lime[900];
+        } else if (lista[index].fase == 2) {
+          cor1 = Colors.amber;
+          cor2 = Colors.amber[900];
+        } else if (lista[index].fase == 3) {
+          cor1 = Colors.green;
+          cor2 = Colors.green[900];
+        }
+        return Card(
+            color: cor1,
+            child: InkWell(
+              splashColor: cor2,
+              onTap: () {
+                Get.to(EditIdeia(lista[index]));
+              },
+              child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text('${lista[index].texto}')
+              ),
+            )
+        );
+      },
       staggeredTileBuilder: (int index) =>
       new StaggeredTile.fit(2),
       mainAxisSpacing: 5.0,
